e7080f3a4f466686534d72db494a14ecff007231
Adrien Le Guillou <git@aleguillou.org>
2025-04-10 15:07:13 +0200

    tests

--- R/00-setup.R
+++ R/00-setup.R
@@ -22,11 +22,6 @@ fs::file_copy(
   overwrite = TRUE
 )
 
-renv::snapshot(
-  packages = c("EpiModelHIV", "EpiModelHPC", "ARTnetData"),
-  lockfile = "renv.lock.hpc"
-)
-
 # # To install the packages from scratch:
 # renv::install(c(
 #     "Epimodel/EpiModelHIV-p",

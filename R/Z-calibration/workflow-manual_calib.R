dea7119090077754b5fbc179de8e987d8dfbc3fd
Adrien Le Guillou <git@aleguillou.org>
2025-04-11 14:22:17 +0200

    update wf order

--- R/Z-calibration/workflow-manual_calib.R
+++ R/Z-calibration/workflow-manual_calib.R
@@ -76,7 +76,6 @@ wf <- add_workflow_step(
       setup_lines = hpc_node_setup
     ),
     sbatch_opts = list(
-      "mail-type" = "END",
       "cpus-per-task" = max_cores,
       "time" = "02:00:00",
       "mem-per-cpu" = "5G"
@@ -91,6 +90,7 @@ wf <- add_workflow_step(
     setup_lines = hpc_node_setup
   ),
   sbatch_opts = list(
+      "mail-type" = "END",
     "cpus-per-task" = max_cores,
     "time" = "02:00:00",
     "mem-per-cpu" = "5G"

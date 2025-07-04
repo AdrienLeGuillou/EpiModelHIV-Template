4e915908d7b486d59e513a2afda9ba8e12571865
Adrien Le Guillou <git@aleguillou.org>
2025-07-04 11:02:48 +0200

    add otc.gfr.stop flag

--- R/F-intervention_scenarios/outcomes.R
+++ R/F-intervention_scenarios/outcomes.R
@@ -59,7 +59,7 @@ mutate_outcomes <- function(d) {
       cml_gfr_drop_yo50 = dbg_gfr_drop_yo50,
 
       lst_prep_any_gfr_lt60 = prep_any_gfr_lt60,
-      lst_prep_std = prep_std_gfr_lt60,
+      lst_prep_std_gfr_lt60 = prep_std_gfr_lt60,
       lst_prep_otc_gfr_lt60 = prep_otc_gfr_lt60,
 
       # HBV --------------------------------------------------------------------

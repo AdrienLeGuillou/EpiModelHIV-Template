c24d26cf7ca071c8ee890e88f4d75f4e2eb225ce
Adrien Le Guillou <git@aleguillou.org>
2025-09-26 21:46:06 +0200

    use klone conf

--- R/hpc_configs.R
+++ R/hpc_configs.R
@@ -12,51 +12,12 @@ mail_user <- "aleguil@emory.edu"
 #   "spack load r@4.4.1"
 # )
 #
-# make_em_workflow <- function(wf_name, override = FALSE) {
-#   wf_path <- paste0("workflows/", wf_name)
-#   if (override && fs::dir_exists(wf_path)) fs::dir_delete(wf_path)
-#
-#   wf <- slurmworkflow::create_workflow(
-#     wf_name = wf_name,
-#     default_sbatch_opts = list(
-#       # "partition" = "preemptable",
-#       "partition" = "epimodel",
-#       "mail-type" = "FAIL",
-#       "mail-user" = mail_user
-#     )
-#   )
-#
-#   # Update RENV on the HPC
-#   wf <- slurmworkflow::add_workflow_step(
-#     wf_summary = wf,
-#     step_tmpl = EpiModelHPC::step_tmpl_renv_restore(
-#       git_branch = current_git_branch,
-#       setup_lines = hpc_node_setup
-#     ),
-#     sbatch_opts = list(
-#       "mem" = "16G",
-#       "cpus-per-task" = 4,
-#       "time" = 180
-#     )
-#   )
-#
-#   return(wf)
-# }
 
 hpc_node_setup <- c(
   ". /gscratch/csde/spack_2025/share/spack/setup-env.sh",
   "spack unload -a",
   "spack load r@4.4"
 )
-
-make_em_workflow <- function(wf_name, override = FALSE, update_renv = TRUE) {
-  if (update_renv) {
-    renv::snapshot(
-      packages = c("EpiModelHIV", "EpiModelHPC", "ARTnetData"),
-      lockfile = "renv.lock.hpc",
-      prompt = FALSE
-    )
-  }
 
 make_em_workflow <- function(wf_name, override = FALSE, update_renv = TRUE) {
   if (update_renv) {

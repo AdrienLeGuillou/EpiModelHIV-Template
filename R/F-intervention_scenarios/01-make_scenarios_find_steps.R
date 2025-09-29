fb11e8541623df037e94879c0f5aeff1b73be32f
Adrien Le Guillou <git@aleguillou.org>
2025-09-29 10:21:56 +0200

    all my finds

--- R/F-intervention_scenarios/01-make_scenarios_find_steps.R
+++ R/F-intervention_scenarios/01-make_scenarios_find_steps.R
@@ -18,6 +18,8 @@ source("R/F-intervention_scenarios/utils-scenarios.R", local = TRUE)
 sc_df_ls <- list()
 sc_names <- c()
 
+# aim num: 16 437
+
 # No OTC +30% ORs: (CLI: 1, OTC: 0)
 #   - CLI: 1.5 (maybe 1.52)
 #   - OTC: 0
@@ -36,8 +38,57 @@ sc_names <- c()
 # test with 0 - 0.75
 
 
-cli_ors <- c(0,    0.3, 0.5, 0.7, 0.9, 1.1, 1.3)
-otc_ors <- c(0.75, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1)
+# new  ORs: (CLI: 0, OTC: 1)
+#   - CLI: 0
+#   - OTC: 0.85
+#
+# new  ORs: (CLI: 0.08, OTC: 0.92)
+#   - CLI: 0.1
+#   - OTC: 0.777
+#
+# new  ORs: (CLI: 0.15, OTC: 0.85)
+#   - CLI: 0.2
+#   - OTC: 0.725
+#
+# new  ORs: (CLI: 0.22, OTC: 0.78)
+#   - CLI: 0.3
+#   - OTC: 0.65
+#
+# new  ORs: (CLI: 0.3 , OTC: 0.7)
+#   - CLI: 0.4
+#   - OTC: 0.6
+#
+# new  ORs: (CLI: 36, OTC: 0.64)
+#   - CLI: 0.5
+#   - OTC: 0.53
+#
+# new  ORs: (CLI: 0.43, OTC: 0.57)
+#   - CLI: 0.6
+#   - OTC: 0.475
+#
+# new  ORs: (CLI: 0.5, OTC: 0.5)
+#   - CLI: 0.7
+#   - OTC: 0.42
+#
+# new  ORs: (CLI: 0.64, OTC: 0.36)
+#   - CLI: 0.9
+#   - OTC: 0.3
+#
+# new  ORs: (CLI: 0.76, OTC: 0.24)
+#   - CLI: 1.1
+#   - OTC: 0.2
+#
+# new  ORs: (CLI: 0.88, OTC: 0.12)
+#   - CLI: 1.3
+#   - OTC: 0.1
+
+
+# cli_ors <- c(0,    0.3, 0.5, 0.7, 0.9, 1.1, 1.3)
+# otc_ors <- c(0.75, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1)
+cli_ors <- c(rep(0.2, 1))
+otc_ors <- c(
+  0.725
+)
 tmp_sc_names <- paste0("otc_cont_cli", cli_ors, "_otc", otc_ors)
 sc_names <- c(sc_names, tmp_sc_names)
 sc_df_ls[["otc_cont"]] <- d_base_otc_best |>

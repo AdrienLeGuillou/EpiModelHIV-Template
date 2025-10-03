d466d13089f420a36951d1522182b07b24d60f87
Adrien Le Guillou <git@aleguillou.org>
2025-10-03 11:27:55 +0200

    ok plot

--- R/F-intervention_scenarios/3-process_plots.R
+++ R/F-intervention_scenarios/3-process_plots.R
@@ -29,8 +29,9 @@ ggplot(d_cont, aes(x = prop_otc, y = tst_rate, col = log(cml_resist))) +
 
 # loess_mod <- loess(log10(cml_resist) ~ prop_otc * tst_rate, data = d_cont, span = 0.25)
 # loess_mod <- loess(cml_resist ~ prop_otc * tst_rate, data = d_cont, span = 0.25)
-loess_mod <- loess(cml_addi_resist_nia ~ prop_otc * tst_rate, data = d_cont, span = 0.75)
-# loess_mod <- loess(cml_pia_all ~ prop_otc * tst_rate, data = d_cont, span = 0.25)
+# loess_mod <- loess(cml_addi_resist_nia ~ prop_otc * tst_rate, data = d_cont, span = 0.75)
+loess_mod <- loess(cml_pia_all ~ prop_otc * tst_rate, data = d_cont, span = 0.25)
+# loess_mod <- loess(cml_nia_all ~ prop_otc * tst_rate, data = d_cont, span = 0.25)
 loess_inter <- expand.grid(list(
   prop_otc = seq(0, 1, 0.01),
   tst_rate = seq(0, 1, 0.01)
@@ -38,15 +39,14 @@ loess_inter <- expand.grid(list(
 loess_inter$z <- as.numeric(predict(loess_mod, newdata = loess_inter))
 loess_inter$x <- loess_inter$prop_otc
 loess_inter$y <- loess_inter$tst_rate
-
-loess_inter$z <- ifelse(loess_inter$z < 0, NA, loess_inter$z)
+# loess_inter$z <- ifelse(loess_inter$z < 0, NA, loess_inter$z)
 
 ggplot(loess_inter, aes(x, y)) +
   geom_raster(aes(fill = z), interpolate = TRUE) +
   geom_contour(aes(z = z), col = "white", alpha = 0.5, lwd = 0.5) +
   theme_classic() +
-  # scale_y_continuous(expand = c(0, 0)) +
-  # scale_x_continuous(expand = c(0, 0)) +
+  scale_y_continuous(expand = c(0, 0)) +
+  scale_x_continuous(expand = c(0, 0)) +
   labs(
     y = "Probability of HIV test before OTC start",
     x = "Proportion of OTC PrEP"

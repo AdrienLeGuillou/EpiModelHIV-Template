f515abd0ea5922c5e2b7140c8ad3e163fb23e738
Adrien Le Guillou <git@aleguillou.org>
2025-10-31 14:45:10 +0100

    cleanup scenarios

--- R/F-intervention_scenarios/0-make_scenarios.R
+++ R/F-intervention_scenarios/0-make_scenarios.R
@@ -29,7 +29,6 @@ sc_df_ls[["baseline"]] <- tibble(
   prep.otc.start.rate_3 = 0
 )
 
-
 # Scenarios exploring changes to relaxed, best and mix --------------------
 name_bases <- c(
   # "only_otc_relaxed_"
@@ -49,39 +48,39 @@ d_bases <- list(
 )
 
 for (i in seq_along(name_bases)) {
-  # # Modify the discontinuation
-  # tmp_sc_names <- paste0(
-  #   name_bases[i],
-  #   "disc_",
-  #   c("025", "050", "075", "100", "125", "150", "175")
-  # )
-  # sc_names <- c(sc_names, tmp_sc_names)
-  # ints_ratios <- c(0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75)
-  # sc_df_ls[[paste0(name_bases[i], "disc")]] <- d_bases[[i]] |>
-  #   slice_sample(n = length(ints_ratios), replace = TRUE) |>
-  #   mutate(
-  #     .scenario.id = tmp_sc_names,
-  #     prep.otc.discont.int_1 = prep.otc.discont.int_1 * ints_ratios,
-  #     prep.otc.discont.int_2 = prep.otc.discont.int_2 * ints_ratios,
-  #     prep.otc.discont.int_3 = prep.otc.discont.int_3 * ints_ratios
-  #   )
-  #
-  # # Modify gfr testing
-  # tmp_sc_names <- paste0(
-  #   name_bases[i],
-  #   "gfr_",
-  #   c("same", "1", "2", "3", "5", "Inf")
-  # )
-  # sc_names <- c(sc_names, tmp_sc_names)
-  # low_gfr_ints <- c(1, 1, 2, 3, 5, Inf)
-  # high_gfr_ints <- c(1 / 2, 1, 2, 3, 5, Inf)
-  # sc_df_ls[[paste0(name_bases[i], "gfr")]] <- d_bases[[i]] |>
-  #   slice_sample(n = length(low_gfr_ints), replace = TRUE) |>
-  #   mutate(
-  #     .scenario.id = tmp_sc_names,
-  #     prep.otc.gfr.low.risk.int = year_steps * low_gfr_ints,
-  #     prep.otc.gfr.high.risk.int = year_steps * high_gfr_ints
-  #   )
+  # Modify the discontinuation
+  tmp_sc_names <- paste0(
+    name_bases[i],
+    "disc_",
+    c("025", "050", "075", "100", "125", "150", "175")
+  )
+  sc_names <- c(sc_names, tmp_sc_names)
+  ints_ratios <- c(0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75)
+  sc_df_ls[[paste0(name_bases[i], "disc")]] <- d_bases[[i]] |>
+    slice_sample(n = length(ints_ratios), replace = TRUE) |>
+    mutate(
+      .scenario.id = tmp_sc_names,
+      prep.otc.discont.int_1 = prep.otc.discont.int_1 * ints_ratios,
+      prep.otc.discont.int_2 = prep.otc.discont.int_2 * ints_ratios,
+      prep.otc.discont.int_3 = prep.otc.discont.int_3 * ints_ratios
+    )
+
+  # Modify gfr testing
+  tmp_sc_names <- paste0(
+    name_bases[i],
+    "gfr_",
+    c("same", "1", "2", "3", "5", "Inf")
+  )
+  sc_names <- c(sc_names, tmp_sc_names)
+  low_gfr_ints <- c(1, 1, 2, 3, 5, Inf)
+  high_gfr_ints <- c(1 / 2, 1, 2, 3, 5, Inf)
+  sc_df_ls[[paste0(name_bases[i], "gfr")]] <- d_bases[[i]] |>
+    slice_sample(n = length(low_gfr_ints), replace = TRUE) |>
+    mutate(
+      .scenario.id = tmp_sc_names,
+      prep.otc.gfr.low.risk.int = year_steps * low_gfr_ints,
+      prep.otc.gfr.high.risk.int = year_steps * high_gfr_ints
+    )
 
   # Modify hiv testing
   tmp_sc_names <- paste0(
@@ -138,50 +137,31 @@ for (i in seq_along(name_bases)) {
     )
 }
 
-# # Scenarios exploring always hiv/sti test in "best" likes ----------------------
-# name_bases <- c(
-#   # "only_otc_best_",
-#   # "otc_mix_",
-#   # "otc_best_",
-#   "otc_indic_"
-# )
-# d_bases <- list(
-#   # d_base_only_otc_best,
-#   # d_base_otc_mix,
-#   # d_base_otc_best,
-#   d_base_otc_indic
-# )
-#
-# for (i in seq_along(name_bases)) {
-#   # tmp_sc_names <- paste0(name_bases[i], "always_stitst")
-#   # sc_names <- c(sc_names, tmp_sc_names)
-#   # sc_df_ls[[paste0(name_bases[i], "always_stitst")]] <- d_bases[[i]] |>
-#   #   slice_sample(n = 1, replace = TRUE) |>
-#   #   mutate(
-#   #     .scenario.id = tmp_sc_names,
-#   #     prep.otc.always.sti.tst = 1
-#   #   )
-#   #
-#   # tmp_sc_names <- paste0(name_bases[i], "always_bothtst")
-#   # sc_names <- c(sc_names, tmp_sc_names)
-#   # sc_df_ls[[paste0(name_bases[i], "always_bothtst")]] <- d_bases[[i]] |>
-#   #   slice_sample(n = 1, replace = TRUE) |>
-#   #   mutate(
-#   #     .scenario.id = tmp_sc_names,
-#   #     prep.otc.always.hiv.tst = 1,
-#   #     prep.otc.always.sti.tst = 1
-#   #   )
-#
-#   hivtst_prob <- c(0, 0.25, 0.5, 0.75, 1)
-#   tmp_sc_names <- paste0(name_bases[i], "some_hivtst_", c(0, 25, 50, 75, 100))
-#   sc_names <- c(sc_names, tmp_sc_names)
-#   sc_df_ls[[paste0(name_bases[i], "some_hivtst")]] <- d_bases[[i]] |>
-#     slice_sample(n = length(hivtst_prob), replace = TRUE) |>
-#     mutate(
-#       .scenario.id = tmp_sc_names,
-#       prep.otc.always.hiv.tst = hivtst_prob
-#     )
-# }
+# Scenarios exploring always hiv/sti test in "best" likes ----------------------
+name_bases <- c(
+  # "only_otc_best_",
+  # "otc_mix_",
+  "otc_best_",
+  "otc_indic_"
+)
+d_bases <- list(
+  # d_base_only_otc_best,
+  # d_base_otc_mix,
+  d_base_otc_best,
+  d_base_otc_indic
+)
+
+for (i in seq_along(name_bases)) {
+  hivtst_prob <- c(0, 0.25, 0.5, 0.75, 1)
+  tmp_sc_names <- paste0(name_bases[i], "some_hivtst_", c(0, 25, 50, 75, 100))
+  sc_names <- c(sc_names, tmp_sc_names)
+  sc_df_ls[[paste0(name_bases[i], "some_hivtst")]] <- d_bases[[i]] |>
+    slice_sample(n = length(hivtst_prob), replace = TRUE) |>
+    mutate(
+      .scenario.id = tmp_sc_names,
+      prep.otc.always.hiv.tst = hivtst_prob
+    )
+}
 
 sc_ls <- lapply(sc_df_ls, EpiModel::create_scenario_list)
 scenarios_list <- Reduce(c, sc_ls, init = list())
